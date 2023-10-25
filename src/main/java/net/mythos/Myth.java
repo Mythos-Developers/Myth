70d6c0a5a059ca6aecaa95eecbf76ea0066d250b
ScienceMind11 <sciencemind@icloud.com>
2023-10-25 13:37:01 -0400

    Some... stuff?

--- src/main/java/net/mythos/Myth.java
+++ src/main/java/net/mythos/Myth.java
@@ -1,10 +1,16 @@
 package net.mythos;
 
 import net.mythos.command.MythosCommand;
+import net.mythos.config.CfgFileEditor;
 import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.loader.api.QuiltLoader;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Myth implements ModInitializer {
 
@@ -15,7 +21,25 @@ public class Myth implements ModInitializer {
 	@Override
 	public void onInitialize(ModContainer mod) {
 
+		try {
+
+			File configDir = new File(QuiltLoader.getConfigDir().toString());
+			File mythDir = new File(QuiltLoader.getConfigDir().toString() + "/myth");
+			File configFile = new File(QuiltLoader.getConfigDir().toString() + "/myth/myth.cfg");
+
+			if(!mythDir.exists()) {
+				CfgFileEditor.create();
+				CfgFileEditor.write();
+			}
+
+			LOGGER.info(String.valueOf(configFile.getTotalSpace()));
+
+		} catch(IOException error) {
+			LOGGER.error("Could not load config file for Myth");
+		}
+
 		LOGGER.info("Successfully loaded and initialized Myth.");
+		LOGGER.info(QuiltLoader.getConfigDir().toString());
 		MythosCommand.register();
 
 	}
